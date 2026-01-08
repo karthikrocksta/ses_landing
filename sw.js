fe560d27237116f47bda4fc2866b25dc0cb7f7ca
karthikrocksta <karthik810@gmail.com>
2026-01-08 22:30:59 +0530

    Initial upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'supreme-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/offline.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/offline.html'))
+  );
+});
